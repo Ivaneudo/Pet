46d4f7763f5f2917fbf6f9d8a956ab69bf02fc2a
Laura Cruz <laurasc1603@gmail.com>
2025-05-28 11:47:14 -0300

    Update Secretaria.php

--- secretaria/Secretaria.php
+++ secretaria/Secretaria.php
@@ -37,7 +37,7 @@ $nomeFuncionario = $_SESSION['usuario'];
                     <li>
                         <a href="SecretariaClientes.php">
                             <div class="icone">
-                                <img src="../img/cliente.png" alt="Clientes">
+                                <img src="../img/cliente.png" alt="cliente"> <!-- Alterado -->
                                 <p>Clientes</p>
                             </div>
                         </a>
@@ -45,7 +45,7 @@ $nomeFuncionario = $_SESSION['usuario'];
                     <li>
                         <a href="SecretariaPet.php">
                             <div class="icone">
-                                <img src="../img/patinha.png" alt="Pets">
+                                <img src="../img/patinha.png" alt="patinha"> <!-- Alterado -->
                                 <p>Pets</p>
                             </div>
                         </a>
@@ -53,7 +53,7 @@ $nomeFuncionario = $_SESSION['usuario'];
                     <li>
                         <a href="SecretariaProdutos.php">
                             <div class="icone">
-                                <img src="../img/estoque.png" alt="Produtos">
+                                <img src="../img/estoque.png" alt="estoque"> <!-- Alterado -->
                                 <p>Produtos</p>
                             </div>
                         </a>
@@ -61,7 +61,7 @@ $nomeFuncionario = $_SESSION['usuario'];
                     <li>
                         <a href="SecretariaServicos.php">
                             <div class="icone">
-                                <img src="../img/caixa.png" alt="Serviços">
+                                <img src="../img/caixa.png" alt="caixa"> <!-- Alterado -->
                                 <p>Serviços</p>
                             </div>
                         </a>
@@ -70,5 +70,42 @@ $nomeFuncionario = $_SESSION['usuario'];
             </nav>
         </div>
     </div>
+    <script>
+        // Mapeia as imagens originais e as imagens de hover
+        const icons = {
+            "cliente": {
+                original: "../img/cliente.png",
+                hover: "../img/cliente-azul.png"
+            },
+            "patinha": {
+                original: "../img/patinha.png",
+                hover: "../img/patinha-azul.png"
+            },
+            "estoque": {
+                original: "../img/estoque.png",
+                hover: "../img/estoque-azul.png"
+            },
+            "caixa": {
+                original: "../img/caixa.png",
+                hover: "../img/caixa-azul.png"
+            }
+        };
+
+        // Função para mudar a imagem ao passar o mouse
+        function changeImage(icon, isHover) {
+            const img = icon.querySelector('img');
+            const iconName = img.alt.toLowerCase(); // Obtém o nome do ícone
+            img.src = isHover ? icons[iconName].hover : icons[iconName].original; // Troca a imagem
+        }
+
+        // Seleciona todos os ícones
+        const iconElements = document.querySelectorAll('.navbar ul a .icone');
+
+        // Adiciona eventos de mouse
+        iconElements.forEach(icon => {
+            icon.addEventListener('mouseenter', () => changeImage(icon, true)); // Muda para a imagem de hover
+            icon.addEventListener('mouseleave', () => changeImage(icon, false)); // Retorna para a imagem original
+        });
+    </script>
 </body>
 </html>
